ac015c968c6d6848d4a46c5135374bbd83f950c4
Abhinand Arun <abhinandarun2015@gmail.com>
2021-11-06 13:16:20 +0530

    Added Files

---
+++ src/Lab_9/Q1/Main.java
@@ -0,0 +1,38 @@
+package Lab_9.Q1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        str = str.toLowerCase();
+        int n = sc.nextInt();
+
+        int[] countArray = new int[26];
+
+        for (int i = 0; i < str.length(); i++) {
+            countArray[str.codePointAt(i) - 97]++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            boolean flag = false;
+
+            for (int j = 0; j < 26; j++) {
+                for (int k = 1; countArray[j] != 0 && k <= countArray[j]; k++) {
+                    if ((j + 1) * k == x) {
+                        System.out.println("YES");
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag) break;
+            }
+            if (!flag) System.out.println("NO");
+        }
+    }
+}
+
---
+++ src/Lab_9/Q2/Main.java
@@ -0,0 +1,22 @@
+package Lab_9.Q2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        boolean flag = true;
+
+        for (int i = 0, j = str.length() - 1; i < str.length() - 1; i++, j--) {
+            if (Math.abs(str.codePointAt(i) - str.codePointAt(i + 1)) != Math.abs(str.codePointAt(j) - str.codePointAt(j - 1))) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) System.out.println("Funny");
+        else System.out.println("Not Funny");
+    }
+}
